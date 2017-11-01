f728b20ff821fd68a980ec55d72e81152b8e8206
Frank Oosterhuis <frank@datprof.com>
2017-11-01 15:32:50 +0100

    New class

---
+++ Challenge/Challenge/Challenges/Challenge323.cs
@@ -0,0 +1,10 @@
+﻿namespace Challenge.Challenges
+{
+    internal class Challenge323 : Challenge
+    {
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
