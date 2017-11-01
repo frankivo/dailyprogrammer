c62e7e62e975f595d1637bf82fb3131ceb5d22ae
Frank Oosterhuis <frank@datprof.com>
2017-11-01 15:37:31 +0100

    Input parser

--- Challenge/Challenge/Challenges/Challenge323.cs
+++ Challenge/Challenge/Challenges/Challenge323.cs
@@ -1,4 +1,8 @@
-﻿namespace Challenge.Challenges
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge.Challenges
 {
     /// <summary>
     /// https://www.reddit.com/r/dailyprogrammer/comments/6melen/20170710_challenge_323_easy_3sum/
@@ -7,7 +11,13 @@
     {
         public override void Execute()
         {
-            throw new System.NotImplementedException();
+            GetInput().ForEach(i => Console.Write($" {i}"));
+            Console.WriteLine();
+            Console.ReadLine();
         }
+
+        private List<int> GetInput() => input.Split(' ').Select(i => int.Parse(i)).ToList();
+
+        private string input = "9 -6 -5 9 8 3 -4 8 1 7 -4 9 -9 1 9 -9 9 4 -6 -8";
     }
 }
