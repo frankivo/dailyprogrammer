24036d5ee3da42d994854134df64ba07213d875b
Frank Oosterhuis <frank@datprof.com>
2017-11-09 12:23:34 +0100

    Read input file

--- Challenge/Challenge/Challenges/Challenge339.cs
+++ Challenge/Challenge/Challenges/Challenge339.cs
@@ -1,10 +1,24 @@
-﻿namespace Challenge.Challenges
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Challenge.Challenges
 {
     internal class Challenge339 : Challenge
     {
+        public Challenge339() => _input = ReadFile("Resources/Challenge339.txt");
+
         public override void Execute()
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine($"Lines: {_input.Count}");
         }
+
+        private static List<string> ReadFile(string path)
+        {
+            return File.ReadAllLines(path).ToList();
+        }
+
+        private readonly List<string> _input;
     }
 }
