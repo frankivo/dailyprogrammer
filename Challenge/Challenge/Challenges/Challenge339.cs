b2586fc40482f7da228496ff41813f2784caa4a1
Frank Oosterhuis <frank@datprof.com>
2017-11-09 12:18:54 +0100

    Init

---
+++ Challenge/Challenge/Challenges/Challenge339.cs
@@ -0,0 +1,10 @@
+﻿namespace Challenge.Challenges
+{
+    internal class Challenge339 : Challenge
+    {
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
