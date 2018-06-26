98df0e8ed633123b61cc6bbb70a2a0383ec5eca1
Frank Oosterhuis <frank@datprof.com>
2018-06-26 14:25:44 +0200

    Solve chal 364

---
+++ Challenge/Challenge/Challenges/Challenge364.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Challenge.Challenges
+{
+    /// <summary>
+    /// https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
+    /// </summary>
+    internal class Challenge364 : Challenge
+    {
+        private readonly Random _random = new Random();
+
+        public override void Execute()
+        {
+            while (true)
+            {
+                var (dice, sides) = GetInput();
+                var output = Calculate(dice, sides);
+                Console.WriteLine($"Output: {output}");
+            }
+        }
+
+        private static (int dice, int sides) GetInput()
+        {
+            Console.WriteLine("Use format '123d123':");
+
+            var input = Console.ReadLine();
+            var parsed = input?.Split('d');
+
+            if (parsed == null || parsed.Length != 2) throw new ArgumentException("Invalid format.");
+
+            return (int.Parse(parsed.First()), int.Parse(parsed.Last()));
+        }
+
+        private  int Calculate(int dice, int sides) => Enumerable.Range(0, dice).Sum(i => _random.Next(1, sides));
+    }
+}
