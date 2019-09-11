9f39f900f0fefd95ed35874bfeedab99318d1f4e
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-11 13:41:13 +0200

    extra comments

--- 380/src/main/scala/bonus2.scala
+++ 380/src/main/scala/bonus2.scala
@@ -3,8 +3,8 @@ object bonus2 {
     helper
       .getFile("https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt")
       .getLines
-      .map(w => (w, morse.smorse(w)))
-      .flatMap(m => {
+      .map(w => (w, morse.smorse(w))) // Calculate morse
+      .flatMap(m => { // Filter morse with 15 dashes in a row
         if (m._2.contains("---------------")) Some(m._1)
         else None
       })
