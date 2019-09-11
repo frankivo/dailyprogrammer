e81f9b38ae9d220bae42195b5b2d987debe1d346
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-11 14:06:28 +0200

    more meaningfull?

--- 380/src/main/scala/bonus2.scala
+++ 380/src/main/scala/bonus2.scala
@@ -4,12 +4,14 @@
 
 object bonus2 {
   def main(args: Array[String]): Unit = {
+    val dash15 = "-" * 15
+
     helper
       .getFile("https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt")
       .getLines
       .map(w => (w, morse.smorse(w))) // Calculate morse
       .flatMap(m => { // Filter morse with 15 dashes in a row
-        if (m._2.contains("---------------")) Some(m._1)
+        if (m._2.contains(dash15)) Some(m._1)
         else None
       })
       .foreach(println)
