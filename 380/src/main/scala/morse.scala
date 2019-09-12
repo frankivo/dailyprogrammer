76edec0daf2a43cffd996a540f95d5f201f83f14
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-12 15:02:18 +0200

    url comment

--- 380/src/main/scala/morse.scala
+++ 380/src/main/scala/morse.scala
@@ -1,3 +1,7 @@
+/*
+  https://www.reddit.com/r/dailyprogrammer/comments/cmd1hb/20190805_challenge_380_easy_smooshed_morse_code_1/
+ */
+
 object morse {
   val charsRaw = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.."
 
