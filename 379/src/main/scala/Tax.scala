622ef9bee117f655c903a50097324ac17cd918c0
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-13 14:55:33 +0200

    wip

--- 379/src/main/scala/Tax.scala
+++ 379/src/main/scala/Tax.scala
@@ -2,8 +2,29 @@
 https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
  */
 
+case class Bracket(incomeCap: Option[Long], taxRate: Double)
+
+case class LimitedBracket(min: Long, max: Long, rate: Double)
+
 object Tax {
-  def tax(amount: Long) : Long = {
+  private val Taxes = List[Bracket](
+    Bracket(Some(10000L), 0.0),
+    Bracket(Some(30000L), 0.1),
+    Bracket(Some(100000L), 0.25),
+    Bracket(None, 0.4),
+  )
+
+  def tax(amount: Long): Long = {
+    Taxes.reverse.foreach(println)
+
     1337L
+  }
+
+  def calcLimits: List[LimitedBracket] = {
+    List()
+  }
+
+  def main(args: Array[String]): Unit = {
+    tax(10010)
   }
 }
