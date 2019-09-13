dc1e11a366c643dcaa56a9e71ef222595de19333
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-13 09:26:06 +0200

    Tests

---
+++ 379/src/main/scala/Tax.scala
@@ -0,0 +1,9 @@
+/*
+https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
+ */
+
+object Tax {
+  def tax(amount: Long) : Long = {
+    1337L
+  }
+}
---
+++ 379/src/test/scala/TestTax.scala
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class TestTax {
+  @Test
+  def amount0(): Unit = assertEquals(0L, Tax.tax(0L))
+
+  @Test
+  def amount10000(): Unit = assertEquals(0L, Tax.tax(10000L))
+
+  @Test
+  def amount10009(): Unit = assertEquals(0L, Tax.tax(10009L))
+
+  @Test
+  def amount10010(): Unit = assertEquals(1L, Tax.tax(10010L))
+
+  @Test
+  def amount12000(): Unit = assertEquals(200L, Tax.tax(12000L))
+
+  @Test
+  def amount56789(): Unit = assertEquals(8697L, Tax.tax(56789L))
+
+  @Test
+  def amount1234567(): Unit = assertEquals(473326L, Tax.tax(1234567L))
+}
