d92c02e3ecb7e8a221d63ee7cbb1ad784872def5
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-23 11:49:15 +0200

    WIP #2

--- 379/src/main/scala/Tax.scala
+++ 379/src/main/scala/Tax.scala
@@ -2,29 +2,42 @@
 https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
  */
 
-case class Bracket(incomeCap: Option[Long], taxRate: Double)
+case class Bracket(incomeCap: Option[Int], taxRate: Double)
 
-case class LimitedBracket(min: Long, max: Long, rate: Double)
+case class LimitedBracket(min: Int, max: Option[Int], rate: Double)
 
 object Tax {
-  private val Taxes = List[Bracket](
-    Bracket(Some(10000L), 0.0),
-    Bracket(Some(30000L), 0.1),
-    Bracket(Some(100000L), 0.25),
-    Bracket(None, 0.4),
+  private val Taxes = List[LimitedBracket](
+    LimitedBracket(0, Some(10000), 0.0),
+    LimitedBracket(10001, Some(30000), 0.1),
+    LimitedBracket(30001, Some(100000), 0.25),
+    LimitedBracket(1000001, None, 0.4)
   )
 
-  def tax(amount: Long): Long = {
-    Taxes.reverse.foreach(println)
+  def tax(amount: Int): Int = {
+    var tax = 0
 
-    1337L
-  }
+    for (i <- Taxes.indices) {
+      val t = Taxes(i)
+      val max = t.max.getOrElse(Int.MaxValue)
 
-  def calcLimits: List[LimitedBracket] = {
-    List()
+      //      println(amount + " : " + max)
+      if (amount > max) {
+        tax = tax + (t.rate * max).toInt
+      }
+      else {
+        val maxSum = Taxes.take(i - 1).map(x => x.max.getOrElse(Int.MaxValue)).sum
+        val toTax = amount - maxSum
+        val newTax = (t.rate * toTax).toInt
+        tax = tax + newTax
+      }
+    }
+
+    tax
   }
 
   def main(args: Array[String]): Unit = {
-    tax(10010)
+    println(tax(40000))
+    //    tax(40000)
   }
 }
--- 379/src/test/scala/TestTax.scala
+++ 379/src/test/scala/TestTax.scala
@@ -3,23 +3,23 @@ import org.junit.jupiter.api.Assertions._
 
 class TestTax {
   @Test
-  def amount0(): Unit = assertEquals(0L, Tax.tax(0L))
+  def amount0(): Unit = assertEquals(0L, Tax.tax(0))
 
   @Test
-  def amount10000(): Unit = assertEquals(0L, Tax.tax(10000L))
+  def amount10000(): Unit = assertEquals(0L, Tax.tax(10000))
 
   @Test
-  def amount10009(): Unit = assertEquals(0L, Tax.tax(10009L))
+  def amount10009(): Unit = assertEquals(0L, Tax.tax(10009))
 
   @Test
-  def amount10010(): Unit = assertEquals(1L, Tax.tax(10010L))
+  def amount10010(): Unit = assertEquals(1L, Tax.tax(10010))
 
   @Test
-  def amount12000(): Unit = assertEquals(200L, Tax.tax(12000L))
+  def amount12000(): Unit = assertEquals(200L, Tax.tax(12000))
 
   @Test
-  def amount56789(): Unit = assertEquals(8697L, Tax.tax(56789L))
+  def amount56789(): Unit = assertEquals(8697L, Tax.tax(56789))
 
   @Test
-  def amount1234567(): Unit = assertEquals(473326L, Tax.tax(1234567L))
+  def amount1234567(): Unit = assertEquals(473326L, Tax.tax(1234567))
 }
