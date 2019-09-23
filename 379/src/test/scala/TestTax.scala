6e1732afcccd1fdfdf224a8474acab794e6a85ce
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-23 15:01:03 +0200

    Readable tests

--- 379/src/test/scala/TestTax.scala
+++ 379/src/test/scala/TestTax.scala
@@ -3,23 +3,23 @@ import org.junit.jupiter.api.Assertions._
 
 class TestTax {
   @Test
-  def amount0(): Unit = assertEquals(0L, Tax.tax(0))
+  def amount0(): Unit = assertEquals(0, Tax.tax(0))
 
   @Test
-  def amount10000(): Unit = assertEquals(0L, Tax.tax(10000))
+  def amount10000(): Unit = assertEquals(0, Tax.tax(10_000))
 
   @Test
-  def amount10009(): Unit = assertEquals(0L, Tax.tax(10009))
+  def amount10009(): Unit = assertEquals(1, Tax.tax(10_009))
 
   @Test
-  def amount10010(): Unit = assertEquals(1L, Tax.tax(10010))
+  def amount10010(): Unit = assertEquals(1, Tax.tax(10_010))
 
   @Test
-  def amount12000(): Unit = assertEquals(200L, Tax.tax(12000))
+  def amount12000(): Unit = assertEquals(200, Tax.tax(12_000))
 
   @Test
-  def amount56789(): Unit = assertEquals(8697L, Tax.tax(56789))
+  def amount56789(): Unit = assertEquals(8_697, Tax.tax(56_789))
 
   @Test
-  def amount1234567(): Unit = assertEquals(473326L, Tax.tax(1234567))
+  def amount1234567(): Unit = assertEquals(473_326, Tax.tax(1_234_567))
 }
