57399f4d669dd89a65aa300b9389a45a63b452c0
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-26 12:06:32 +0200

    Fit1

--- 377/src/main/scala/Boxes.scala
+++ 377/src/main/scala/Boxes.scala
@@ -9,20 +9,5 @@ You can fit 4 boxes along the x-axis (because 6*4 <= 25), and 3 boxes along the 
 case class Point(x: Int, y: Int)
 
 object Boxes {
-  def fit1(crate: Point, box: Point): Int = {
-    0
-  }
-
-  def main(args: Array[String]): Unit = {
-    List(
-      (Point(25, 18), Point(6, 5)),
-      (Point(10, 10), Point(1, 1)),
-      (Point(12, 34), Point(5, 6)),
-      (Point(12345, 678910), Point(1112, 1314)),
-      (Point(5, 100), Point(6, 1))
-    ).foreach(x => {
-      println(x)
-      println(fit1(x._1, x._2))
-    })
-  }
+  def fit1(crate: Point, box: Point): Int = (crate.x / box.x) * (crate.y / box.y)
 }
