14fce79e3f26c10b6be8e47910e8b94f0c0e0516
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-28 13:31:18 +0200

    Warmup3 tests

--- 378/src/main/scala/main.scala
+++ 378/src/main/scala/main.scala
@@ -9,4 +9,10 @@ object main {
   // Given a sequence of answers, return the sequence sorted in descending order,
   // so that the first number is the largest and the last number is the smallest.
   def warmup2(numbers: Array[Int]): Array[Int] = numbers.sorted.reverse
+
+  // Given a number N and a sequence of answers,
+  // return true if N is greater than the number of answers (i.e. the length of the sequence),
+  // and false if N is less than or equal to the number of answers.
+  // For instance, given 7 and [6, 5, 5, 3, 2, 2, 2], you would return false, because 7 is less than or equal to 7.
+  def warmup3(count: Int, numbers: Array[Int]): Boolean = false
 }
