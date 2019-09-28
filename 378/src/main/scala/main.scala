334c425b14987e3221c7d17ec63d43c3ba5ec732
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-09-28 13:32:39 +0200

    Warmup3

--- 378/src/main/scala/main.scala
+++ 378/src/main/scala/main.scala
@@ -13,6 +13,5 @@ object main {
   // Given a number N and a sequence of answers,
   // return true if N is greater than the number of answers (i.e. the length of the sequence),
   // and false if N is less than or equal to the number of answers.
-  // For instance, given 7 and [6, 5, 5, 3, 2, 2, 2], you would return false, because 7 is less than or equal to 7.
-  def warmup3(count: Int, numbers: Array[Int]): Boolean = false
+  def warmup3(count: Int, numbers: Array[Int]): Boolean = count > numbers.length
 }
