689d0c2a5843c2d206ae66cbf6dd99a44a4ce844
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-10-01 09:20:10 +0200

    generate

--- 197/src/main/scala/ISBN10.scala
+++ 197/src/main/scala/ISBN10.scala
@@ -1,5 +1,7 @@
+import scala.util.Random
+
 object ISBN10 {
-  def isISBN(isbn: String): Boolean = {
+  def validate(isbn: String): Boolean = {
     isbn
       .toUpperCase
       .filter(x => x.isDigit || x.equals('X'))
@@ -9,5 +11,20 @@ object ISBN10 {
       .map(n => (n._1, n._2 + 1))
       .map(n => n._1 * n._2)
       .sum % 11 == 0
+  }
+
+  def generate: String = {
+    val code = (1 to 9).map(_ => Random.nextInt(9))
+    val sum = code
+      .reverse
+      .zipWithIndex
+      .map(x => (x._1, x._2 + 2))
+      .map(z => z._1 * z._2)
+      .sum
+    val rest = (0 to 10)
+      .filter(i => (sum + i) % 11 == 0)
+      .map(x => if (x == 10) 'X' else x)
+      .head
+    code.mkString + rest
   }
 }
