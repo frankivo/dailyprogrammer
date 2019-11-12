da55dc102a2df7420bad93af5e91bd0157e8009a
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-11-12 11:37:13 +0100

    this is now a scala project :)

---
+++ 381/src/main/scala/Yatzee.scala
@@ -0,0 +1,9 @@
+/*
+  https://www.reddit.com/r/dailyprogrammer/comments/dv0231/20191111_challenge_381_easy_yahtzee_upper_section/
+ */
+
+object Yatzee {
+  def main(args: Array[String]): Unit = {
+    println("yatzee!")
+  }
+}
