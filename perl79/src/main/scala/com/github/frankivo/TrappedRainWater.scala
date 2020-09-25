a35c938d9dde2bd337c0c5a565f27dcdcada745e
Frank Oosterhuis <frank+github@scriptzone.nl>
2020-09-25 22:07:00 +0200

    print histogram

--- perl79/src/main/scala/com/github/frankivo/TrappedRainWater.scala
+++ perl79/src/main/scala/com/github/frankivo/TrappedRainWater.scala
@@ -4,6 +4,24 @@ package com.github.frankivo
 
 object TrappedRainWater {
   def getAmount(water: Array[Int]): Int = {
+    printHistogram(water)
     0
+  }
+
+  def printHistogram(water: Array[Int]): Unit = {
+    val height = water.max
+    val indices = (1 to height).reverse
+
+    (1 to height).foreach(i => {
+      val curHeight = indices(i - 1)
+
+      print(indices(i - 1))
+      water.foreach(w => print(" %s".format(if (curHeight <= w) "#" else " ")))
+
+      println
+    })
+
+    println("- " * (water.length + 1))
+    println(" " * 2 + water.mkString(" "))
   }
 }
