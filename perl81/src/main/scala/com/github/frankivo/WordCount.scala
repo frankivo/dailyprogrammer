817cf8964f10d953878f6d760555a1687a758c45
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-10-06 10:54:33 +0200

    Counts per word

--- perl81/src/main/scala/com/github/frankivo/WordCount.scala
+++ perl81/src/main/scala/com/github/frankivo/WordCount.scala
@@ -3,11 +3,17 @@ package com.github.frankivo
 object WordCount {
 
   def countWords(text: String): List[(Int, Seq[String])] = {
-    println(text)
+    val clean = cleanText(text)
+    val words = toWords(clean)
 
+    val counts = words.groupMapReduce(identity)(_ => 1)(_ + _)
+    println(counts)
 
-    List(
-
-    )
+    List()
   }
+
+  def cleanText(input: String): String = input.replaceAll("(\\.|\"|\\(|\\)|,|'s|--)", "")
+
+  def toWords(input: String): Seq[String] = input.split(" ").filter(_.length > 0)
+
 }
